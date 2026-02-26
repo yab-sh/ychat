9249e47a769fbe523a8ce76282850b15a8c037d5
CasuallyDreamin <ysn.arambash@gmail.com>
2026-02-26 15:07:24 -0500

    feat: Implementing Routing and Bootstrap

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { initTheme } from "@/utils/theme.ts";
 import './index.css'
+
 import App from './App.tsx'
 
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from '@/context/AuthContext.tsx'
+import { WebSocketProvider } from "@/context/WebSocketContext.tsx";
+import { SettingsProvider } from './context/SettingsContext.tsx';
+
+initTheme()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <WebSocketProvider>
+        <SettingsProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </SettingsProvider>
+      </WebSocketProvider>
+    </AuthProvider>
   </StrictMode>,
 )
